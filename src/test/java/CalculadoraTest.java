5e1909eab2cbe70c8af7233568a5fc3c8484498b
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-02-28 17:03:57 -0300

    test: add methods with negative numbers

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -11,12 +11,25 @@ public class CalculadoraTest {
         double result = calculadora.calcular(5.0, 3.0, 'A');
         assertEquals(8.0, result, 0.0001);
     }
+    @Test
+    public void testAdicaoComNegativos() {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.calcular(-5.0, -3.0, 'A');
+        assertEquals(-8.0, result, 0.0001);
+    }
 
     @Test
     public void testSubtracao() {
         Calculadora calculadora = new Calculadora();
         double result = calculadora.calcular(5.0, 3.0, 'S');
         assertEquals(2.0, result, 0.0001);
+    }
+
+    @Test
+    public void testSubtracaoComNegativos() {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.calcular(-5.0, -3.0, 'S');
+        assertEquals(-2.0, result, 0.0001);
     }
 
     @Test
@@ -27,10 +40,38 @@ public class CalculadoraTest {
     }
 
     @Test
+    public void testMultiplicacaoComNegativos() {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.calcular(-5.0, -3.0, 'M');
+        assertEquals(15.0, result, 0.0001);
+    }
+
+    @Test
+    public void testMultiplicacaoComPositivoENegativo() {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.calcular(-5.0, 3.0, 'M');
+        assertEquals(-15.0, result, 0.0001);
+    }
+
+    @Test
     public void testDivisao() {
         Calculadora calculadora = new Calculadora();
         double result = calculadora.calcular(6.0, 3.0, 'D');
         assertEquals(2.0, result, 0.0001);
+    }
+
+    @Test
+    public void testDivisaoComNegativos() {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.calcular(-6.0, -3.0, 'D');
+        assertEquals(2.0, result, 0.0001);
+    }
+
+    @Test
+    public void testDivisaoComPositivoENegativo() {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.calcular(-6.0, 3.0, 'D');
+        assertEquals(-2.0, result, 0.0001);
     }
 
     @Test
